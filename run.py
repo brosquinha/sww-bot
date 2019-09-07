32908b3671faea2a651082ec2c4ae6f44ef1924a
Thales César <thalesgiriboni@gmail.com>
2019-09-07 20:35:26 -0300

    Adapt run.py to run any available script

--- run.py
+++ run.py
@@ -1,4 +1,11 @@
+import sys
+
+from bot.scripts import aparicoes
 from bot.scripts import emuso
 
 if __name__ == "__main__":
-    emuso.main()
+    script = sys.argv[1] if len(sys.argv) > 1 else input("Script to run: ")
+    try:
+        eval(script).main()
+    except NameError:
+        print("Script {} does not exist".format(script))
