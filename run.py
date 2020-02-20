0174e03efb59e4dfa62bd5024ca5f1e3427838f5
Thales César <thalesgiriboni@gmail.com>
2020-02-20 14:50:32 -0300

    Refactor run.py with importlib

--- run.py
+++ run.py
@@ -1,12 +1,10 @@
 import sys
-
-from bot.scripts import aparicoes
-from bot.scripts import emuso
-from bot.scripts import linha_do_tempo
+from importlib import import_module
 
 if __name__ == "__main__":
     script = sys.argv[1] if len(sys.argv) > 1 else input("Script to run: ")
     try:
-        eval(script).main()
-    except NameError:
+        script_module = import_module('bot.scripts.{}'.format(script))
+        script_module.main()
+    except ModuleNotFoundError:
         print("Script {} does not exist".format(script))
